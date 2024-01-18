e0c5a693e3cf422bb7ebfbd28ed15002469fc561
Aayush Bhattarai <bhattaraiaayush.78@gmail.com>
2024-01-18 02:55:24 +0200

    applied if else statement and disabling the box so that user cannot change their value!

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset");
+var resetBtn = document.querySelector("#reset");
 
 
-let turnO = true; //playerX, playerO
-//
-const winningPatterns = [
+ let turnO = true;  //playerX, playerO
+ const winningPatterns = [
     [0, 1, 2],
     [0, 3, 6],
     [0, 4, 8],
@@ -13,4 +12,31 @@ const winningPatterns = [
     [2, 4, 6],
     [3, 4, 5],
     [6, 7, 8],
-  ];
+  ]; 
+
+ boxes.forEach(function(boxey){
+    boxey.addEventListener("click", ()=>{
+        
+        if (turnO){      //same as turnO === true
+            boxey.innerText = "O";
+            turnO = false;
+        }
+        else{
+            boxey.innerText = "X";
+            turnO = true;
+        }
+
+//disables the interactivity of the boxey element, 
+//typically used to prevent further clicks or interactions on that element.
+//prevent repetitive clicks in the game so that players cannot change their value again
+        boxey.disabled=true;  
+
+    
+        checkWinner();
+    });
+});
+
+const checkWinner
+
+
+
